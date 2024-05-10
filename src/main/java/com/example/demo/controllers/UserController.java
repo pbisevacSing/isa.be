40865fd973db20f91d85430595df3c9a88a02d6a
simpel-transport-pb <pbisevac@singimail.rs>
2024-05-10 17:51:46 +0200

    Add model validators :: Check User Model and UserController

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.UserModel;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +28,11 @@ public class UserController {
     }
 
     @PostMapping("create-user-body")
-    public UserModel createUserBody(@RequestBody UserModel userModel) {
-        return userModel;
+    public ResponseEntity<?> createUserBody(@RequestBody @Valid UserModel userModel, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>("Neuspesno registrovan!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<UserModel>(userModel, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/demo/models/UserModel.java
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -1,12 +1,19 @@
 package com.example.demo.models;
 
+import com.example.demo.validators.ContactNumberConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserModel {
     private int id;
+
+    @NotBlank
     private String firstName;
     private String lastName;
+    @Email
     private String email;
+    @ContactNumberConstraint
     private String contactNumber;
 }
