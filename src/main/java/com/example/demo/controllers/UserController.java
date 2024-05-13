3a408e90bb423e5ff907eb3bc77fe2bd33fc02fe
simpel-transport-pb <pbisevac@singimail.rs>
2024-05-13 17:36:47 +0200

    Code for 2024-05-13

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.User;
+import com.example.demo.mappers.UserMapper;
 import com.example.demo.models.UserModel;
+import com.example.demo.models.UserPageModel;
 import com.example.demo.repositories.IUserRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -24,9 +28,14 @@ public class UserController {
         return "Petar";
     }
 
-    @GetMapping("get-first-name-list")
-    public List<User> getFirstNameList() {
-        return userRepository.findAll();
+    @GetMapping("get-user-list")
+    public List<UserModel> getUserList() {
+        return UserMapper.toModelList(userRepository.findAll());
+    }
+
+    @GetMapping("get-user-page-list")
+    public UserPageModel getUserPageList(Integer pageNumber, Integer pageSize) {
+        return UserMapper.toModelPagedList(userRepository.findAll(PageRequest.of(pageNumber, pageSize)));
     }
 
     @PostMapping("create-user")
@@ -39,6 +48,10 @@ public class UserController {
         if (result.hasErrors()) {
             return new ResponseEntity<>("Neuspesno registrovan!", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+        var entity = UserMapper.toEntity(userModel);
+
+        userRepository.save(entity);
 
         return new ResponseEntity<UserModel>(userModel, HttpStatus.CREATED);
     }
---
+++ src/main/java/com/example/demo/mappers/UserMapper.java
@@ -0,0 +1,45 @@
+package com.example.demo.mappers;
+
+import com.example.demo.entities.User;
+import com.example.demo.models.UserModel;
+import com.example.demo.models.UserPageModel;
+import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserMapper {
+    public static User toEntity(UserModel model) {
+        User user = new User();
+        user.setId(model.getId());
+        user.setFirstName(model.getFirstName());
+        user.setLastName(model.getLastName());
+        user.setEmail(model.getEmail());
+        return user;
+    }
+
+
+    public static UserModel toModel(User entity) {
+        return UserModel.builder()
+                .id(entity.getId())
+                .email(entity.getEmail())
+                .firstName(entity.getFirstName())
+                .lastName(entity.getLastName()).build();
+    }
+
+    public static List<UserModel> toModelList(List<User> entities) {
+        var list = new ArrayList<UserModel>();
+        for (var entity : entities) {
+            list.add(toModel(entity));
+        }
+        return list;
+    }
+
+    public static UserPageModel toModelPagedList(Page<User> pageEntity) {
+       return UserPageModel.builder()
+               .Users(toModelList(pageEntity.getContent()))
+               .totalPages(pageEntity.getTotalPages())
+               .totalElements(pageEntity.getNumberOfElements())
+               .build();
+    }
+}
