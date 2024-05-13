decbb26e4be74930931ecd47e82c28d799dafe9d
simpel-transport-pb <pbisevac@singimail.rs>
2024-05-13 16:51:37 +0200

    .

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -4,13 +4,12 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(schema = "users")
+@Table(name = "users")
 @Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+    private Integer id;
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
--- src/main/java/com/example/demo/repositories/IUserRepository.java
+++ src/main/java/com/example/demo/repositories/IUserRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repositories;
 import com.example.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserRepository extends JpaRepository<User, Long> {
+public interface IUserRepository extends JpaRepository<User, Integer> {
 }
