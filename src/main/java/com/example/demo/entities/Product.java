32decb789fde03a1ffd6913e6c7ff489108933e2
simpel-transport-pb <pbisevac@singimail.rs>
2024-05-17 16:27:59 +0200

    .

---
+++ src/main/java/com/example/demo/entities/Product.java
@@ -0,0 +1,17 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "user_id")
+    private String user_id;
+}
