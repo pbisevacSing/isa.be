91c69a248a95e5f7ad53e11de000a4434536437b
simpel-transport-pb <pbisevac@singimail.rs>
2024-05-22 18:23:39 +0200

    Auth models and service

---
+++ src/main/java/com/example/demo/models/LoginResponseModel.java
@@ -0,0 +1,8 @@
+package com.example.demo.models;
+
+import lombok.Data;
+
+@Data
+public class LoginResponseModel {
+    private String token;
+}
---
+++ src/main/java/com/example/demo/services/AuthenticationService.java
@@ -0,0 +1,47 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.User;
+import com.example.demo.models.LoginUserModel;
+import com.example.demo.models.RegisterUserModel;
+import com.example.demo.repositories.IUserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final IUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            IUserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserModel input) {
+        User user = new User();
+        user.setEmail(input.getEmail());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserModel input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
